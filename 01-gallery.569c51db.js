2e16cebf1d0b190ece9b2a3758d97f48d41aa8f1
Perec1983 <131287323+Perec1983@users.noreply.github.com>
2023-09-18 19:15:47 +0000

    Deploying to gh-pages from @ Perec1983/goit-js-hw-09@8c23bf7abf5a8c99c8984309ef93865914980843 🚀

---
+++ 01-gallery.569c51db.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),a=null;e.setAttribute("disabled",!0),t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),a=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){e.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(a)}))}();
+//# sourceMappingURL=01-gallery.569c51db.js.map
