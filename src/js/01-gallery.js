8c23bf7abf5a8c99c8984309ef93865914980843
Perec1983 <mr.malahov2013@gmail.com>
2023-09-18 22:15:15 +0300

    yrtt

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const changeColorBody = document.querySelector('body');
+let timerId = null;
 
-console.log(galleryItems);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+  
+  stopBtn.setAttribute('disabled', true);
+
+  function startClickBtn() {
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+    timerId = setInterval(() => {
+    changeColorBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  }
+
+  startBtn.addEventListener('click', startClickBtn);
+  
+  function stopClickBtn() {
+    stopBtn.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled');
+    clearInterval(timerId);
+  }
+  
+  stopBtn.addEventListener('click', stopClickBtn);
+  
